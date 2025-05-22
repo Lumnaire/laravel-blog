d8b7e14baba49649efe6f296b962ce32be367488
Lumnaire <callme14noy@gmail.com>
2025-05-22 21:51:13 +0800

    categorytabs changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,9 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-4 text-gray-900">
-                    <x-category-tabs />
+                    <x-category-tabs>
+                        No Categories
+                    </x-category-tabs>
 
                 </div>
             </div>
