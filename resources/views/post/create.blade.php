8504fa992924e86c2489c02d25b652e87219df15
Lumnaire <callme14noy@gmail.com>
2025-05-26 23:39:54 +0800

    added add post textarea

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -3,7 +3,24 @@
     <div class="py-4">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
-                Create Post
+                <form action="{{ route('post.store') }}" method="post">
+
+                    <!-- Title -->
+                    <div>
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
+                            :value="old('title')" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+
+                    <!-- Content -->
+                    <div class="mt-4">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-input-textarea id="content" class="block mt-1 w-full" type="text" name="content"
+                            :value="old('content')" required autofocus></x-input-textarea>
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+                </form>
             </div>
         </div>
     </div>
