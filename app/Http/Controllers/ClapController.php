f55c26ab1c6a2548241e266ec24192653a1b0c44
Ronald Castromero <callme14noy@gmail.com>
2025-07-17 17:15:20 +0800

    fixed some of claps logic

--- app/Http/Controllers/ClapController.php
+++ app/Http/Controllers/ClapController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class ClapController extends Controller
 {
     public function clap(Post $post)
     {
+        $hasClapped = auth()->user()->hasClapped($post);
+
+        if($hasClapped) {
+            $post->claps()->where('user_id', auth()->id())->delete();
+        } else {
+           $post->claps()->create([
+             'user_id' => auth()->id(),
+           ]);
+        }
+
+        return response()->json([
+            'clapsCount' => $post->claps()->count(),
+        ]);
+
         $post->claps()->create([
             'user_id' => auth()->id(),
         ]);
